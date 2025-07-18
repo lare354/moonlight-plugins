b58d9de0dd29df08517aa7cb5f5de8c881418879
lare354 <lare354@duck.com>
2025-07-18 03:39:43 +0200

    add missing require() statement. Add support for call buttons in DMs (thabk you nanakusa)

--- src/doubleClickToJoin/index.tsx
+++ src/doubleClickToJoin/index.tsx
@@ -2,21 +2,23 @@ import { ExtensionWebpackModule, Patch } from "@moonlight-mod/types";
 
 export const patches: Patch[] = [
   {
+      // voice channels
     find: ".handleVoiceStatusClick",
     replace: [
       {
-          match: /onClick:\(\)=>\{this.handleClick\(\)/g,
-          replacement: 'onClick:()=>{require("doubleClickToJoin_action")?.schedule(()=>{this.handleClick()},this)',  
-        },
+        match: /onClick:\(\)=>\{this.handleClick\(\)/g,
+        replacement: 'onClick:()=>{require("doubleClickToJoin_action")?.schedule(()=>{this.handleClick()},this)',  
+      },
     ]
   },
   {
+    // stage channels
     find: ".handleClickChat",
     replace: [
       {
-          match: /onClick:\(\)=>\{this.handleClick\(\)/g,
-          replacement: 'onClick:()=>{require("doubleClickToJoin_action")?.schedule(()=>{this.handleClick()},this)',  
-        },
+        match: /onClick:\(\)=>\{this.handleClick\(\)/g,
+        replacement: 'onClick:()=>{require("doubleClickToJoin_action")?.schedule(()=>{this.handleClick()},this)',  
+      },
     ]
   },
   {
@@ -25,9 +27,34 @@ export const patches: Patch[] = [
     replace: {
         match: /onClick:(\i)(?=,.{0,30}className:"channelMention".+?(\i)\.inContent)/,
         replacement: (_, onClick, props) => "" 
-          + `onClick:(vcDoubleClickEvt)=>shouldRunOnClick(vcDoubleClickEvt,${props})&&${onClick}()`,
+          + `onClick:(vcDoubleClickEvt)=>require("doubleClickToJoin_action")?.shouldRunOnClick(vcDoubleClickEvt,${props})&&${onClick}()`,
      }
-  }
+  },
+  {
+    // call icons in DM (thank you Nanakusa :3)
+    find: ".renderVoiceCallButton",
+    replace: [
+      {
+        match: "onClick:this.handleVoiceClick",
+        replacement: "onClick:()=>null,onDoubleClick:this.handleVoiceClick",
+      },
+      {
+        match: /(this\.handleBrowserNotSupported.{50,75})onClick:/,
+        replacement: (_orig: string, prefix: string) => `${prefix}onClick:()=>null,onDoubleClick:`,
+      },
+    ],
+  },
+  {
+    // call icons in DM (thank you Nanakusa :3)
+    find: ".Masks.HEADER_BAR_BADGE_TOP", 
+    replace: [ 
+      { 
+        match: /onClick:(\w+\?void 0:)\w+,/, 
+        replacement: (prefix, condition: string) => 
+        `${prefix}onDoubleClick:${condition}arguments[0]?.onDoubleClick,`, 
+      }, 
+    ], 
+   }
 ];
 
 export const webpackModules: Record<string, ExtensionWebpackModule> = {
